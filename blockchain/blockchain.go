5adc3d0159fdd42eafdc3f2c40a4c3cfb7070025
Muhamad Risman <real.risman@gmail.com>
2022-04-23 13:02:47 +0700

    feat: add iterator function for blockchain

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -15,6 +15,11 @@ type BlockChain struct {
 	Database *badger.DB
 }
 
+type BlockChainIterator struct {
+	CurrentHash []byte
+	Database    *badger.DB
+}
+
 func InitBlockChain() *BlockChain {
 	var lastHash []byte
 
@@ -72,4 +77,28 @@ func (chain *BlockChain) AddBlock(data string) {
 		return err
 	})
 	Handle(err)
+}
+
+func (chain *BlockChain) Iterator() *BlockChainIterator {
+	iter := &BlockChainIterator{chain.LastHash, chain.Database}
+
+	return iter
+}
+
+func (iter *BlockChainIterator) Next() *Block {
+	var block *Block
+
+	err := iter.Database.View(func(txn *badger.Txn) error {
+		item, err := txn.Get(iter.CurrentHash)
+		Handle(err)
+		encodedBlock, err := item.ValueCopy(nil)
+		block = Deserialize(encodedBlock)
+
+		return err
+	})
+	Handle(err)
+
+	iter.CurrentHash = block.PrevHash
+
+	return block
 }
