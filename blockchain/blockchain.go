91d34f11f70b3bf9525f8040d29920ce2ba9f2ac
Muhamad Risman <real.risman@gmail.com>
2022-04-23 12:54:30 +0700

    refactor: blockchain to store the hash into badger db

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,15 +1,75 @@
 package blockchain
 
+import (
+	"fmt"
+
+	"github.com/dgraph-io/badger/v3"
+)
+
+const (
+	dbPath = "./tmp/blocks"
+)
+
 type BlockChain struct {
-	Blocks []*Block
+	LastHash []byte
+	Database *badger.DB
 }
 
 func InitBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genenis()}}
+	var lastHash []byte
+
+	opts := badger.DefaultOptions(dbPath)
+
+	db, err := badger.Open(opts)
+	Handle(err)
+
+	err = db.Update(func(txn *badger.Txn) error {
+		if _, err := txn.Get([]byte("lh")); err == badger.ErrKeyNotFound {
+			fmt.Println("No existing blockchain found")
+			genesis := Genenis()
+			fmt.Println("Genesis proved")
+			err = txn.Set(genesis.Hash, genesis.Serialize())
+			Handle(err)
+			err = txn.Set([]byte("lh"), genesis.Hash)
+
+			lastHash = genesis.Hash
+
+			return err
+		} else {
+			item, err := txn.Get([]byte("lh"))
+			Handle(err)
+			lastHash, err = item.ValueCopy(nil)
+			return err
+		}
+	})
+	Handle(err)
+
+	blockchain := BlockChain{lastHash, db}
+	return &blockchain
 }
 
 func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.Blocks[len(chain.Blocks)-1]
-	new := CreateBlock(data, prevBlock.Hash)
-	chain.Blocks = append(chain.Blocks, new)
+	var lastHash []byte
+
+	err := chain.Database.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte("lh"))
+		Handle(err)
+		lastHash, err = item.ValueCopy(nil)
+
+		return err
+	})
+	Handle(err)
+
+	newBlock := CreateBlock(data, lastHash)
+
+	err = chain.Database.Update(func(txn *badger.Txn) error {
+		err := txn.Set(newBlock.Hash, newBlock.Serialize())
+		Handle(err)
+		err = txn.Set([]byte("lh"), newBlock.Hash)
+
+		chain.LastHash = newBlock.Hash
+
+		return err
+	})
+	Handle(err)
 }
